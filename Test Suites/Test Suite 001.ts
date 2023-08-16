33eaba87db43637c9124c867f5f423967961cec4
Snoo Chew <snoochew@gmail.com>
2023-08-17 10:11:00 +1200

    test suite

---
+++ Test Suites/Test Suite 001.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite 001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>72689ad1-928a-42cc-a8c7-1b7577681ebd</testSuiteGuid>
+</TestSuiteEntity>
